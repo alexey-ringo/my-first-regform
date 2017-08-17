b332b82358728c55003cebc7b78508cd5c754c0f
Alex Ringo <alexey.ringo@gmail.com>
2017-08-17 13:21:35 +0000

    upload from cloud 9

---
+++ includes/constants.php
@@ -0,0 +1,7 @@
+<?php
+	// Константы базы данных
+	define("DB_SERVER", "mysql.dr-solomko.myjino.ru");
+	define("DB_USER", "045790012_mywork");
+	define("DB_PASS", "y4md564703");
+	define("DB_NAME", "dr-solomko_mywork");
+	?>
---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+© 2014 <a href="http://www.childrencorner.ru/">childrencorner.ru</a>. Все права защищены.
+
+</footer>
+</body>
+</html>
